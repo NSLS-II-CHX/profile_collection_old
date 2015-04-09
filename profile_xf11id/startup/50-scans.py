df43b7fc089abb2fff87841173c28b3f5849a3b1
CHX Operator <xf11id@xf11id-ws1.cs.nsls2.local>
2015-04-09 17:05:51 -0400

    ENH: Define a CHX Ascan class.

--- profile_xf11id/startup/50-scans.py
+++ profile_xf11id/startup/50-scans.py
@@ -8,3 +8,13 @@ dscan = DScan()
 # Use ct as a count which is a single scan.
 
 ct = Count()
+
+class CHXAScan(AScan):
+    def __call__(self, *args, **kwargs):
+        super(CHXAScan, self).__call__(*args, **kwargs)
+
+    def post_scan(self):
+        print('pos[0], det[0]: {}, {}'.format(
+            self.positioners[0].name, self.detectors[0].name))
+        est = estimate(self.positioners[0].name, self.detectors[0].name)
+        print('estimates: {}'.format(est))
