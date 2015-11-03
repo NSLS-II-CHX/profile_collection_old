809f41a3402e20d88a37d7252f06df2fcedcb9c1
CHX Operator <xf11id@xf11id-ws1.cs.nsls2.local>
2015-11-03 17:01:31 -0500

    [FIX] add read-method

--- profile_collection/startup/30-user.py
+++ profile_collection/startup/30-user.py
@@ -2,12 +2,19 @@ from ophyd.controls import EpicsMotor
 
 class ReversedEpicsMotor(EpicsMotor):
     
-	def move(self, position, wait=True, **kwargs):
-		return super().move(-position, wait, **kwargs)
+    def move(self, position, wait=True, **kwargs):
+        return super().move(-position, wait, **kwargs)
 
-	@property
-	def position(self):
-		return -super().position
+    @property
+    def position(self):
+        return -super().position
+
+    def read(self):
+        vals = super().read()
+        print('vals = %s' % vals)
+        for k, v in vals.items():
+            v['value'] = -v['value']
+        return vals
 
 diff_xv2_r = ReversedEpicsMotor('XF:11IDB-ES{Dif-Ax:XV2}Mtr', name='diff_xv2_r')
 
