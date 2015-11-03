e30c05a922e063f06be0c364a9c0ee4844e6ad72
Eric Dill <edill@bnl.gov>
2015-11-03 14:47:53 -0500

    Spaces not tabs, argh!

--- profile_collection/startup/99-bluesky.py
+++ profile_collection/startup/99-bluesky.py
@@ -1,12 +1,12 @@
 
 def detselect(detector_object, suffix="_stats_total1"):
-	"""Switch the active detector and set some internal state"""
+    """Switch the active detector and set some internal state"""
     gs.DETS =[detector_object]
     gs.PLOT_Y = detector_object.name + suffix
     gs.TABLE_COLS = [gs.PLOT_Y] 
 
 
 def movr(positioner, position):
-	"""Move a positioner relative to its current position"""
-	mov(positioner, positioner.position + position)
+    """Move a positioner relative to its current position"""
+    mov(positioner, positioner.position + position)
 
