e3d1c8bbf238516d7a10e03aea0fbd378c4a4f6f
Eric Dill <edill@bnl.gov>
2015-11-16 17:48:43 -0500

    ENH: Add 'better' plotting control for live plots

--- profile_collection/startup/99-bluesky.py
+++ profile_collection/startup/99-bluesky.py
@@ -5,3 +5,19 @@ def detselect(detector_object, suffix="_stats_total1"):
     gs.PLOT_Y = detector_object.name + suffix
     gs.TABLE_COLS = [gs.PLOT_Y] 
 
+def chx_plot_motor(scan):
+    fig = None
+    if gs.PLOTMODE == 1:
+        fig = plt.gcf()
+    elif gs.PLOTMODE == 2:
+        fig = plt.gcf()
+        fig.clear()
+    elif gs.PLOTMODE == 3:
+        fig = plt.figure()
+    return LivePlot(gs.PLOT_Y, scan.motor._name, fig=fig)
+
+dscan.default_sub_factories['all'][1] = chx_plot_motor
+
+gs.PLOTMODE = 1
+
+from bluesky.global_state import resume, abort, stop, panic, all_is_well, state
