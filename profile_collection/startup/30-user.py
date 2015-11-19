b6a4cb6fad4b36fefddb48a3ce0ef21be09aa000
Daron Chabot <dchabot@bnl.gov>
2015-11-19 10:35:23 -0500

    [ENH] user convenience functions

--- profile_collection/startup/30-user.py
+++ profile_collection/startup/30-user.py
@@ -16,7 +16,32 @@ class ReversedEpicsMotor(EpicsMotor):
             v['value'] = -v['value']
         return vals
 
-diff_xv2_r = ReversedEpicsMotor('XF:11IDB-ES{Dif-Ax:XV2}Mtr', name='diff_xv2_r')
+# keep this line as example!!!! bst_x = ReversedEpicsMotor('XF:11IDB-ES{Dif-Ax:XV2}Mtr', name='bst_x')
+#bst_x = ReversedEpicsMotor('XF:11IDB-ES{Dif-Ax:XV2}Mtr', name='bst_x')
+bst_y = ReversedEpicsMotor('XF:11IDB-ES{Dif-Ax:YV}Mtr', name = 'bst_y')
+#bst_rot = diff_om
 
 # Alias motors
-sam_tth = diff_thh
+bst_x = diff_xv2
+sam_x = diff_xh
+sam_y = diff_yh
+#sam_z = diff_zh
+#sam_th = diff_thh
+#sam_chi = diff_chh
+sam_pitch = diff_phh
+
+def att(num):
+	if num == 0:
+		mov(bst_x,18.)
+		mov(bst_y,.17)
+	elif num == 1:
+		mov(bst_x,25.)
+		mov(bst_y,.17)
+	elif num == 2:
+		mov(bst_x,31.)
+		mov(bst_y,.17)
+
+def W_in():
+	mov(bst_x,-12.5)
+	mov(bst_y,-.6298)
+
