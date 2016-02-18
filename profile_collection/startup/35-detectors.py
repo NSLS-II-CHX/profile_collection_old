49e86b4300eccc0c99817f51a93dd558b06319a4
Daron Chabot <dchabot@bnl.gov>
2016-02-18 17:51:07 -0500

    ENH: add xbpm Device

---
+++ profile_collection/startup/35-detectors.py
@@ -0,0 +1,16 @@
+from ophyd import (Device, Component as Cpt, EpicsSignalRO)
+
+
+class XBpm(Device):
+    x = Cpt(EpicsSignalRO, 'Pos:X-I')
+    y = Cpt(EpicsSignalRO, 'Pos:Y-I')
+    a = Cpt(EpicsSignalRO, 'Ampl:ACurrAvg-I')
+    b = Cpt(EpicsSignalRO, 'Ampl:BCurrAvg-I')
+    c = Cpt(EpicsSignalRO, 'Ampl:CCurrAvg-I')
+    d = Cpt(EpicsSignalRO, 'Ampl:DCurrAvg-I')
+    
+
+
+
+xbpm = XBpm('XF:11IDB-BI{XBPM:02}', name='xbpm')
+xbpm.read_attrs = ['x', 'y', 'a', 'b', 'c', 'd']
