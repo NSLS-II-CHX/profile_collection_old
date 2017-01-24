b2adc19e5f28c16bc7cfcd38ed35043d7b1fbe29
Yugang Zhang <yuzhang@bnl.gov>
2017-01-24 17:31:09 -0500

    ADD readback.name in iuv_gap to fix speck save prob

--- profile_collection/startup/15-machines.py
+++ profile_collection/startup/15-machines.py
@@ -12,7 +12,9 @@ class Undulator(PVPositionerPC):
     stop_value = 1
 
 ivu_gap = Undulator('SR:C11-ID:G1{IVU20:1', name='ivu_gap')
-ivu_gap.readback = 'ivu_gap'
+# ivu_gap.readback = 'ivu_gap'   ####what the ^*(*()**)(* !!!!
+#To solve the "KeyError Problem" when doing dscan and trying to save to a spec file, Y.G., 20170110
+ivu_gap.readback.name = 'ivu_gap' 
 
 # This class is defined in 10-optics.py
 fe = VirtualMotorCenterAndGap('FE:C11A-OP{Slt:12', name='fe') # Front End Slits (Primary Slits)
