ac02d9b4a4798085103473797fb5da40cab31644
Yugang Zhang <yuzhang@bnl.gov>
2017-01-31 17:13:17 -0500

    DEV funcs for point_detector commissioning

---
+++ profile_collection/startup/93-point_detector.py
@@ -0,0 +1,110 @@
+"""
+Developed at Jan 31, 2017 for Point Detector Commissioning
+"""
+
+count_PV =  'XF:11IDB-BI{IM:1}:C1'
+integer_time = 10 #10 seconds
+acquisition_period = 0.1 #seconds
+
+HV_enable_PV = 'XF:11IDB-BI{IM:1}:ENBHIV1' 
+HV_set_PV = 'XF:11IDB-BI{IM:1}:NEW_HIV1'
+HV_set_action_PV = 'XF:11IDB-BI{IM:1}:SETHIVS'
+Init_PV =  'XF:11IDB-BI{IM:1}:INIT_DAQ'
+
+low_v_PV = 'XF:11IDB-BI{IM:1}:NEW_LO1'  #-1 e.g.
+low_v_set_PV = 'XF:11IDB-BI{IM:1}:SETLOS'
+
+high_v_PV = 'XF:11IDB-BI{IM:1}:NEW_HI1' #-2 e.g.
+high_v_set_PV = 'XF:11IDB-BI{IM:1}:SETHIS'
+
+
+
+def set_HV( hv ):
+        '''
+        Set the applied high voltage
+        '''
+        caput( HV_enable_PV, 1)
+        caput( HV_set_PV , hv )
+        caput( HV_set_action_PV, 1)
+
+
+
+def get_pdet_count( integer_time, acquisition_period  ):
+        '''
+        get the averaged count during integer_time
+        '''
+        Nt = int( integer_time/acquisition_period )
+        data = 0 
+        for i in range( Nt ):
+                time.sleep( acquisition_period ) 
+                data += caget( count_PV)
+        return data/Nt
+
+def get_count_hv( hv, low_v, high_v ):
+        '''get count for different attenuation at one applied high voltage
+         Example:
+                #for LaBr
+                data = get_count_hv( -2000, -1.05, -1.45 )
+        '''
+        set_HV(hv)        
+        data = np.zeros( len(att_n) )
+        caput( low_v_PV, low_v)
+        caput( low_v_set_PV, 1)
+        caput( high_v_PV, high_v)
+        caput( high_v_set_PV, 1)
+        
+        for i, transm in enumerate(att_real):                
+                att.set_T(transm)
+                caput( Init_PV, 1)
+                time.sleep( 30 )
+                data[i] = get_pdet_count(10, 1) #do 10 seconds average
+                print( "The count for %s HV wit attenuation as %s is: %s" %(hv, transm, data[i] ) )
+        att.set_T(1)
+        return data
+
+def get_count_hv_series( hvs ):
+        '''
+        get count for different attenuation at different applied high voltage
+        Example:
+        #for LaBr
+        data = get_count_hv_series( HV_LB  )
+        '''
+        data = {}
+        for i, hv in enumerate( hvs ):
+                print( '*'*40)
+                print( 'The current applied voltage is: %s'%hv )
+                print( '*'*40)
+                low_v = max( vol_LB[i] - v_window, 0 )
+                high_v = min(vol_LB[i] + v_window, 5 )
+                data[hv] = get_count_hv( hv, low_v, high_v )
+        return data
+
+
+att_n = np.array(       [1,     .9,     0.7,   0.5,    0.4,    0.3,       0.2,    0.1,  0.05, 0.025, 0.01, 0.005,  0.001    ]                                           )
+att_real =  np.array(  [ 1,   0.812,   0.66,  0.54,   0.44,   0.288,      0.19,   0.1,  0.0546, 0.024, 0.01, 0.0045, 0.001       ])
+
+#for LaBr3 detector
+HV_LB = -np.linspace( 1100, 2000, 10)
+#         array([  1100.,1200.,1300.,1400.,1500.,  1600.,  1700.,  1800.,  1900.,  2000.])
+vol_LB = np.array(  [0.04, 0.26, 0.58, 1.12, 1.17, 1.2, 1.21, 1.22, 1.24, 1.25 ]  )
+v_window = .2
+
+
+hv = -2000 
+hvs = HV_LB
+
+data_dir ='/XF11ID/analysis/2017_1/commissioning/Results/Point_Detector/'
+#np.savetxt( data_dir + 'count_2000V.txt', np.vstack( [att_real, data] ) )
+
+#For LaBr, other metadata: Triger Mode: INTermal; Intergration time: 0.1 second;Dwell time 1ms; Deadtime: 20 ns
+
+
+        
+#plot1D( x = att_real, y = data, c='r', m = 'o', ls='--', legend='LaBr@2000', title='LaBr detector@2000V', xlabel='attenuation', ylabel='count' )        
+
+
+
+
+
+
+        
