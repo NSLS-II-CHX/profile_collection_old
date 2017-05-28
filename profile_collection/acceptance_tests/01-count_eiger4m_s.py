4f5d7452f809a5fb749c4b860f74b928848703ca
yugangzhang <yuzhang@bnl.gov>
2017-05-28 11:14:36 -0400

    ADD: Show one image of the scan

--- profile_collection/acceptance_tests/01-count_eiger4m_s.py
+++ profile_collection/acceptance_tests/01-count_eiger4m_s.py
@@ -3,10 +3,13 @@ from bluesky.callbacks import LiveTable, LivePlot
 
 
 
-for aq_t, aq_p in zip([1, 1], [1, 2]):
+for aq_t, aq_p in zip([1, 1], [1, 1]):
     eiger4m_single.cam.acquire_time.value = aq_t
     eiger4m_single.cam.acquire_period.value = aq_p
-    eiger4m_single.cam.num_images.value = 10 
-    print("describe what to see")
+    eiger4m_single.cam.num_images.value = 5
+    eiger4m.tr.num_images.value = 1
+    print("collect five images")
     RE(Count([eiger4m_single]), 
        LiveTable(['eiger4m_single_stats1_total', 'eiger4m_single_stats2_total']))
+
+img = get_images(db[-1], 'eiger4m_single_image');print('show the first image');plt.imshow( img[0][0] )
