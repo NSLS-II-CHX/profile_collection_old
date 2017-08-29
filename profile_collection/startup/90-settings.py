bd3d97cefe61886ab8c2fa24eecd624ca1c6f751
yugangzhang <yuzhang@bnl.gov>
2017-08-29 11:05:48 -0400

    Remove redundant Scan ID printing (there is another one elsewhere)

--- profile_collection/startup/90-settings.py
+++ profile_collection/startup/90-settings.py
@@ -6,13 +6,6 @@ RE.md['beamline_id'] = 'CHX'
 # removing 'custom' as it is raising an exception in 0.3.2
 # gs.RE.md['custom'] = {}
 
-
-
-def print_scanid(name, doc):
-    if name == 'start':
-        print('Scan ID:', doc['scan_id'])
-        print('Unique ID:', doc['uid'])
-
 def print_md(name, doc):
     if name == 'start':
         print('Metadata:\n', repr(doc))
